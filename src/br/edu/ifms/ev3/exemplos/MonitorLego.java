af016238c080a42b2cec62b07a36a99ab1f23055
Max <max.mjm.melo@gmail.com>
2019-06-07 10:34:28 -0400

    RMI ev3

--- src/br/edu/ifms/ev3/exemplos/MonitorLego.java
+++ src/br/edu/ifms/ev3/exemplos/MonitorLego.java
@@ -4,6 +4,8 @@ import java.net.MalformedURLException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.remote.ev3.RMIRegulatedMotor;
 import lejos.remote.ev3.RMISampleProvider;
 import lejos.remote.ev3.RemoteEV3;
 
@@ -11,6 +13,8 @@ public class MonitorLego {
 	
 	private RMISampleProvider sampleProvider = null;
 	private RemoteEV3 ev3 = null;
+	private RMIRegulatedMotor me;
+	private RMIRegulatedMotor md;
 
 	public MonitorLego() {
 		
@@ -25,6 +29,24 @@ public class MonitorLego {
 			
 			System.out.println("cor -> "+ sample);
 			
+			me = ev3.createRegulatedMotor("A", 'L');
+			md = ev3.createRegulatedMotor("B", 'L');
+			
+			//me.setSpeed(100);
+			//md.setSpeed(100);
+			
+			me.rotateTo(360, true);
+			md.rotateTo(360, true);
+			
+			while(me.isMoving());
+			
+			me.forward();
+			md.forward();			
+			
+			
+			me.stop(true);
+			md.stop(true);
+			
 		} catch (RemoteException | MalformedURLException | NotBoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -32,6 +54,12 @@ public class MonitorLego {
 		finally {
 			try {
 				sampleProvider.close();
+				
+				me.close();
+				md.close();
+				
+				
+				
 			} catch (RemoteException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
