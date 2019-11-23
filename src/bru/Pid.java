53a5eb06466b6ee8b937a872ac19e10bc50b3936
Max <max.mjm.melo@gmail.com>
2019-11-23 17:07:34 -0400

    correção identação

--- src/bru/Pid.java
+++ src/bru/Pid.java
@@ -12,38 +12,39 @@ public class Pid {
 	public Pid (){
 		EV3LargeRegulatedMotor motorB = new EV3LargeRegulatedMotor(MotorPort.B);
 		EV3LargeRegulatedMotor motorC = new EV3LargeRegulatedMotor(MotorPort.C);
-		
+		int sp,at,erro;
+    	int vetor[], i,kp=13;
+    	vetor = new int[2];
 		
         ColorSensor s2 = new ColorSensor(SensorPort.S2);
         
-       s2.setRedMode();
+        s2.setRedMode();
        
-       motorB.forward();;
+        motorB.forward();;
         motorC.forward();
         System.out.println("passou desta linha");
-       while(Button.ESCAPE.isUp()) {
-       int sp,at,erro;
-       int vetor[], i,kp=13;
-       vetor = new int[2];
+        while(Button.ESCAPE.isUp()) {
+        	
       
-       //preto = 4;
-       //branco = 33;
-       sp=18;
-       at=(int) (s2.getAmbient()*100);
-       erro = sp-at;
+	       //preto = 4;
+	       //branco = 33;
+	       sp=18;
+	       at=(int) (s2.getAmbient()*100);
+	       erro = sp-at;
        
-	    	   vetor[0]=16 - erro;
-		       vetor[1]= 16 + erro;
-		       for(i=0;i<=1;i++) {
-		    	   vetor[i]=vetor[i]*kp;
-		    	   System.out.println(vetor[i]);
-		       }
-		       motorB.setSpeed(vetor[0]);
-		        motorC.setSpeed(vetor[1]);
+	       vetor[0]=16 - erro;
+		   vetor[1]= 16 + erro;
+		   
+		   for(i=0;i<=1;i++) {
+			   vetor[i]=vetor[i]*kp;
+		    	System.out.println(vetor[i]);
+		   }
+		   motorB.setSpeed(vetor[0]);
+		   motorC.setSpeed(vetor[1]);
 		        
 		        
 	      
-	       }
+        }
 	}
 	
 	
